210633a2748a5820b176689358692e6dde724b1d
weslleyolli <weslleyrichardson.cg@hotmail.com>
2024-08-02 11:42:55 -0300

    Theme toggle and accounts menu

--- pizza-shop-web/src/components/header.tsx
+++ pizza-shop-web/src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -19,6 +21,10 @@ export function Header() {
             <p className="font-medium">Order</p>
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
