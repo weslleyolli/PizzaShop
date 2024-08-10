95f48e8647521c3397d94d91d8ff263e906760f4
weslleyolli <weslleyrichardson.cg@hotmail.com>
2024-08-10 12:20:33 -0300

    Revenue graph during the period

---
+++ pizza-shop-web/src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,69 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '10/12', revenue: 1200 },
+  { date: '11/12', revenue: 800 },
+  { date: '12/12', revenue: 900 },
+  { date: '13/12', revenue: 400 },
+  { date: '14/12', revenue: 1700 },
+  { date: '15/12', revenue: 700 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Revenue during the period
+          </CardTitle>
+          <CardDescription>Daily revenue during the period </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={248}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'EUR',
+                })
+              }
+            />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet['500']}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
